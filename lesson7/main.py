d34c02a0dac85117661628be8026328bdb5ea083
aba0911 <jumanasiti77@gamail.com>
2023-06-20 11:20:08 +0700

    update lesson7

---
+++ lesson7/main.py
@@ -0,0 +1,8 @@
+
+from method import describeFunction, printNama, bababa
+from read import readUser
+
+printNama("aba taher assabirin")
+bababa()
+describeFunction("aba", "depok", 17)
+readUser()
---
+++ lesson7/read.py
@@ -0,0 +1,5 @@
+def readUser():
+    userData = ["abataher", "tamam", "eca"]
+
+    for i in userData:
+        print(i)
